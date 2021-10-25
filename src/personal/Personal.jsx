5ead09a72e2e9d5ba9a6b3f2df95567efa5d487f
SiriusSchrodinger <hw.yang@berkeley.edu>
2021-10-25 14:21:34 -0700

    personal

--- src/personal/Personal.jsx
+++ src/personal/Personal.jsx
@@ -3,8 +3,27 @@ import "./personal.css"
 export default function Personal() {
     return (
         <>
-            <div className="personalTitle">
-                Personal
+            <div className="personal">
+                <div className="personalTitle">
+                    Personal
+                </div>
+                <div className="personalContainer">
+                    <div className="personalText">
+                        <p>
+                            As a proud member of the LGBT+ community,
+                            I was a volunteer for Queer Cal Pals, a Berkeley program that 
+                            help new students connect with  LGBTQ+ community, 
+                            as well as provide personal and social support during 
+                            their transition to life at Cal. 
+                        </p>
+                        <br />
+                        <p>
+                            I also manage a public official account that posts articles 
+                            that promote equal rights ideas and organize events for 
+                            gender minority communities in China. 
+                        </p>
+                    </div>
+                </div>
             </div>
         </>
     )
