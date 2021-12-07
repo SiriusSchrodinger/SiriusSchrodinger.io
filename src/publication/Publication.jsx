31a6fbf05a8c819ea7eb862b1a60507e113cc247
SiriusSchrodinger <hw.yang@berkeley.edu>
2021-12-06 22:56:08 -0800

    style

--- src/publication/Publication.jsx
+++ src/publication/Publication.jsx
@@ -3,11 +3,11 @@ import './publication.css';
 export default function Publication() {
     return (
         <>
-        <div className="publication">
-            <div className="publicationTitle">
+        <div className="pub">
+            <div className="pubTitle">
                 Prototypical Model with Novel Information-theoretic Loss Function for Generalized Zero Shot Learning
             </div>
-            <div className="publicationAbstact">
+            <div className="pubAbstact">
                 We propose three information-theoretic loss functions for 
                 deterministic Generalized zero shot learning models: 
                 a mutual information loss to bridge seen data and target classes; 
@@ -16,8 +16,9 @@ export default function Publication() {
                 learn the embedding of target classes; 
                 a semantic preserving cross entropy loss to 
                 preserve the semantic relation when mapping the semantic representations to 
-                the common space.
+                the common space. <a className="pubHref" href="https://arxiv.org/abs/2112.03134" target="_blank">arXiv:2112.03134</a>
             </div>
+            
         </div>
         
         </>
